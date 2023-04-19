25f99d9dea3a27d029e78193d94c60a5278ea98e
luzinashikorina <114040901+luzinashikorina@users.noreply.github.com>
2023-04-19 16:02:45 +0300

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-print ARGV[0].scan(/\[from:(.*?)\]/).join
-print (",")
-print ARGV[0].scan(/\[to:(.*?)\]/).join
-print (",")
-print ARGV[0].scan(/\[flags:(.*?)\]/).join
-print ("\n")
